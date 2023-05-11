a824414562ea45e46ebbbc20d4ec2f059ce367d2
KaranMishra <93134411+Karan-Kumar-Mishra@users.noreply.github.com>
2023-05-11 11:55:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ let i=0;
 const io=require('socket.io')(http)
 io.on('connection',(socket)=>{
     socket.emit('get-users',users_in_chat);
-    
+    console.log('connected....')
     socket.on('joins',(name)=>{
-        
+        console.log(name+' join the chat..');
         u_name=name;
         users_in_chat.push(name);
         socket.broadcast.emit('come',name);       
@@ -29,8 +29,8 @@ io.on('connection',(socket)=>{
     socket.on('disconnect',function()
     {
         socket.broadcast.emit('left',u_name);
-       
+        console.log("disconnected...")
         users_in_chat.pop();
     })
-    
+    console.log("live user => "+users_in_chat);
 })
